57241760bba9521ecefef6b50dbaf5f687c3db5e
Tamiwo <taimwo.otutama@gmail.com>
2018-03-04 01:27:15 +0900

    update:ちょっと整理
    super.initまでに非オプショナル型の初期値を設定しておく必要がある
    継承元クラスのプロパティが扱えるのはsuper.init以降

--- Meteor/Sources/Views/Detail/GaugeView.swift
+++ Meteor/Sources/Views/Detail/GaugeView.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  GaugeView.swift
 //  Meteor
 //
 //  Created by Kazuaki Oe on 2018/03/01.
@@ -12,39 +12,43 @@ class GaugeView: SKSpriteNode {
     let meteorGaugeMask: SKShapeNode
     
     init(frame: CGRect) {
-        let meteorGaugeSprite = SKSpriteNode(imageNamed: "meteorGauge")
-        meteorGaugeMask = SKShapeNode(rect: CGRect(x: 0, y: 0,
-                                                   width: meteorGaugeSprite.size.width * 2,
-                                                   height: meteorGaugeSprite.size.height * 2))
-        meteorGaugeMask.position.x = -meteorGaugeSprite.size.width
-        meteorGaugeMask.position.y = -meteorGaugeSprite.size.height
-        let baseTexture = SKTexture(imageNamed: "gaugeBase")
-        super.init(texture: nil, color: UIColor.clear, size:baseTexture.size())
         //ゲージベース枠
-        let base = SKSpriteNode(texture: baseTexture)
-        addChild(base)
+        let base = SKSpriteNode(imageNamed: "gaugeBase")
+        base.name = "base"
         //ゲージ背景追加
         let back = SKSpriteNode(imageNamed: "gaugeBack")
         back.name = "back"
         //透けてる部分のサイズ分しか背景がないので引き延ばす
         back.xScale = base.size.width / back.size.width
         back.yScale = base.size.height / back.size.height
-        addChild(back)
-        //meteorゲージ
-        meteorGaugeMask.fillColor = UIColor.red
-        let meteorGauge = SKCropNode()
-        meteorGauge.maskNode = meteorGaugeMask
+        //メテオゲージSpriteNode
+        let meteorGaugeSprite = SKSpriteNode(imageNamed: "meteorGauge")
         meteorGaugeSprite.xScale = 2.0
         meteorGaugeSprite.yScale = 2.0
-        meteorGauge.position.x = 60
+        //メテオゲージ調整用マスク
+        meteorGaugeMask = SKShapeNode(rect: CGRect(x: 0, y: 0,
+                                                   width: meteorGaugeSprite.size.width * 2,
+                                                   height: meteorGaugeSprite.size.height * 2))
+        meteorGaugeMask.position.x = -meteorGaugeSprite.size.width
+        meteorGaugeMask.position.y = -meteorGaugeSprite.size.height
+        meteorGaugeMask.fillColor = UIColor.red
+        //メテオゲージ
+        let meteorGauge = SKCropNode()
+        meteorGauge.maskNode = meteorGaugeMask
+        meteorGauge.position.x = 60 //思考錯誤で決めたゲージの位置
         meteorGauge.addChild(meteorGaugeSprite)
+        //継承元クラスの初期化
+        super.init(texture: nil, color: UIColor.clear, size:base.size)
+        //ノード追加
         addChild(meteorGauge)
-        //zPosion
+        addChild(back)
+        addChild(base)
+        //zPosion設定
         zPosition = 1000
         back.zPosition = zPosition + 0.1
         meteorGauge.zPosition = zPosition + 0.2
         base.zPosition = zPosition + 0.3
-        //縮尺を合わせる
+        //スケール調整
         xScale = 0.58
         yScale = 0.58
         //フレームの一番下に配置する
