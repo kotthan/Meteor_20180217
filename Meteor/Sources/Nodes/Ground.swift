f1e3c59f13282e20451adb847670c45083649886
Tamiwo <taimwo.otutama@gmail.com>
2018-03-03 14:12:34 +0900

    地面のphysicsBodyの初期化処理

--- Meteor/Sources/Nodes/Ground.swift
+++ Meteor/Sources/Nodes/Ground.swift
@@ -15,12 +15,15 @@ class Ground: SKNode
     init(frame: CGRect)
     {
         super.init()
-        Ground = SKShapeNode(rect: CGRect(x: 0, y: 139.135, width: frame.size.width, height: 100))
+        Ground = SKShapeNode(rect: CGRect(x: 0, y: 139.135, width: frame.size.width, height: 1))
         Ground.fillColor = UIColor.red
         Ground.name = "ground"
         Ground.position.x = 0
         Ground.position.y = 0
         Ground.zPosition = 1000
+        Ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: frame.size.width,
+                                                               height: 1g),
+                                           center: CGPoint(x: 0, y: 139.135))
         Ground.physicsBody?.categoryBitMask = 0b0001
         Ground.physicsBody?.collisionBitMask = 0b0000 | 0b0000
         Ground.physicsBody?.contactTestBitMask = 0b0100
