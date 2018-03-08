04babcef92ed857953058ac5520ad4968d572481
Tamiwo <taimwo.otutama@gmail.com>
2018-03-09 00:20:52 +0900

    update:Meteorクラス追加

---
+++ Meteor/Sources/Nodes/Meteor.swift
@@ -0,0 +1,13 @@
+//
+//  Meteor.swift
+//  Meteor
+//
+//  Created by Ryota on 2018/03/09.
+//  Copyright © 2018年 Kazuaki Oe. All rights reserved.
+//
+
+import SpriteKit
+
+class Meteor: SKNode{
+    
+}
