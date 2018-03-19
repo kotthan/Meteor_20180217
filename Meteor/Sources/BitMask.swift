3c51a3987b3156e9711af20d26109b0772451e7e
Tamiwo <taimwo.otutama@gmail.com>
2018-03-19 23:37:06 +0900

    fix:不要なファイル削除
    gameSceneは昔のやつが整理前の場所にいたので削除

--- Meteor/Sources/BitMask.swift
+++
@@ -1,8 +0,0 @@
-enum BitMask: Int{
-    case None           = 0b00000000
-    case Player         = 0b00000001 
-    case Meteor         = 0b00000010
-    case Ground         = 0b00000100
-    case AttackShape    = 0b00001000
-    case GuardShape     = 0b00010000
-}
