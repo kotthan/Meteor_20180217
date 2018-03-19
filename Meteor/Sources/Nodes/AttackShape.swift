aec142fdf44e48277beebc093ac87627b8224c7d
Tamiwo <taimwo.otutama@gmail.com>
2018-03-19 23:41:54 +0900

    fix:AttackShapeクラスとGuardshapeクラス作成

---
+++ Meteor/Sources/Nodes/AttackShape.swift
@@ -0,0 +1,13 @@
+//
+//  AttackShape.swift
+//  Meteor
+//
+//  Created by Ryota on 2018/03/19.
+//  Copyright © 2018年 Kazuaki Oe. All rights reserved.
+//
+
+import SpriteKit
+
+class AttackShape: SKSpriteNode {
+    
+}
---
+++ Meteor/Sources/Nodes/GuardShape.swift
@@ -0,0 +1,13 @@
+//
+//  GuardShape.swift
+//  Meteor
+//
+//  Created by Ryota on 2018/03/19.
+//  Copyright © 2018年 Kazuaki Oe. All rights reserved.
+//
+
+import SpriteKit
+
+class GuardShape: SKSpriteNode {
+    
+}
