69d4bccbfe7ddee53ad7dae0a179b56a39eabd3a
Tamiwo <taimwo.otutama@gmail.com>
2018-03-21 17:29:55 +0900

    fix:PlayerSpriteクラス追加

---
+++ Meteor/Sources/Nodes/PlayerSprite.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerSprite.swift
+//  Meteor
+//
+//  Created by Ryota on 2018/03/21.
+//  Copyright © 2018年 Kazuaki Oe. All rights reserved.
+//
+
+import SpriteKit
+
+class PlayerSprite: SKSpriteNode {
+    
+}
