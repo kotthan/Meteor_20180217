7bff4d4386df900d25e92b6d5ad80620a978d933
Tamiwo <taimwo.otutama@gmail.com>
2018-03-26 23:39:35 +0900

    fix:ゲームオーバ画面を別Sceneに

--- Meteor/Sources/Scenes/GameScene.swift
+++ Meteor/Sources/Scenes/GameScene.swift
@@ -57,7 +57,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var highScore = 0                                               //ハイスコア
     //MARK: 画面
     var pauseView: PauseView!                                       //ポーズ画面
-    var gameOverView: GameOverView!
     var hudView = HUDView()                                         //HUD
     
     var mainBgmPlayer: AVAudioPlayer!
@@ -428,10 +427,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 case let node where node?.name == "BackTitle":
                     gameFlg = true
                     self.player.ultraAttack()
-                case let node where node == gameOverView?.HomeButton :
-                    homeButtonAction()
-                case let node where node ==  gameOverView?.ReStartButton :
-                    reStartButtonAction()
                 case let node where node == pauseButton?.PauseButton :
                     pauseButton.tapped()
                 default:
@@ -781,15 +776,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             pauseButton.isHidden = true//ポーズボタンを非表示にする
             hudView.scoreLabel.isHidden = true
             hudView.highScoreLabel.isHidden = true
-            //ハイスコア更新
-            print("------------score:\(self.score) high:\(self.highScore)------------")
-            if( self.score > self.highScore ){
-                self.highScore = self.score
-                self.highScoreLabel.text = String(self.highScore)
-                print("------------hidh score!------------")
-                UserDefaults.standard.set(self.highScore, forKey: self.keyHighScore) //データの保存
-            }
-            print("------------gameover------------")
             self.mainBgmPlayer.stop()
             //墜落演出
             let circle = SKShapeNode(circleOfRadius:1)
@@ -810,60 +796,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                         },
                       ]),
                   SKAction.run {
-                    self.gameOverView = GameOverView(frame: self.frame, score: self.score, highScore: self.highScore )
-                    self.camera?.addChild(self.gameOverView)
+                    let gameOverScene = GameOverScene(size: self.frame.size)
+                    gameOverScene.setScore(score: self.score, highScore: self.highScore)
+                    self.view?.presentScene(gameOverScene)
                     },
                   //SKAction.run{self.isPaused = true},
-                  SKAction.run{self.gameOverView.isHidden = false},
 
                 ])
             circle.run(actions)
         }
-    }
-
-    func newGame()
-    {
-        var gameScene: GameScene!
-        if (UIDevice.current.model.range(of: "iPad") != nil) {
-            gameScene = GameScene(size: CGSize(width: 375.0, height: 667.0))
-            gameScene.scaleMode = .fill
-        }
-        else{
-            gameScene = GameScene(size: frame.size)
-            gameScene.scaleMode = .aspectFill
-        }
-        self.view?.presentScene(gameScene)
-    }
-    
-    func homeButtonAction()
-    {
-        let actions = SKAction.sequence([
-            SKAction.run { self.playSound("push_45") },
-            SKAction.run { self.gameOverView.audioPlayer.stop() },
-            SKAction.run { adBanner.isHidden = true },
-            SKAction.run { self.newGame() }
-            ])
-        run(actions)
-    }
-    func reStartButtonAction()
-    {
-        var gameScene: GameScene!
-        if (UIDevice.current.model.range(of: "iPad") != nil) {
-            gameScene = GameScene(size: CGSize(width: 375.0, height: 667.0))
-            gameScene.scaleMode = .fill
-        }
-        else{
-            gameScene = GameScene(size: frame.size)
-            gameScene.scaleMode = .aspectFill
-        }
-        gameScene.retryFlg = true
-        let actions = SKAction.sequence([
-            SKAction.run { self.playSound("push_45") },
-            SKAction.run { self.gameOverView.audioPlayer.stop() },
-            SKAction.run { adBanner.isHidden = true },
-            SKAction.run { self.view!.presentScene(gameScene)}
-            ])
-        run(actions)
     }
     
     func playBgm(soundName: String)
