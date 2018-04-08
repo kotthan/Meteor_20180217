1a8a874b416d809bf166fc92a652b264983c1cb6
Tamiwo <taimwo.otutama@gmail.com>
2018-04-08 11:25:54 +0900

    fix:隕石の横位置をランダムに

--- Meteor/Sources/Nodes/XPosition.swift
+++ Meteor/Sources/Nodes/XPosition.swift
@@ -14,3 +14,25 @@ enum XPositon: CGFloat {
     case center = 187.5
     case right = 281.25
 }
+
+//ランダムな値を返す参考
+//https://qiita.com/noppefoxwolf/items/d216479dcc66431d7136
+extension XPositon: EnumRandomized {
+    static var all: [XPositon] {
+        return [left, center, right]
+    }
+}
+
+protocol EnumRandomized {
+    static var all: [Self] { get }
+    static var random: Self? { get }
+}
+
+extension EnumRandomized {
+    static var random: Self? {
+        let all = self.all
+        if all.isEmpty { return nil }
+        let index = Int(arc4random_uniform(UInt32(all.count)))
+        return all[index]
+    }
+}
