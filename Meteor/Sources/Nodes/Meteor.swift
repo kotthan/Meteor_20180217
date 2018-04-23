99f21d0a52853d3db96b3835532eac9ef65e9983
Tamiwo <taimwo.otutama@gmail.com>
2018-04-23 23:09:53 +0900

    update:弱点の位置に合わせてヒビの位置をかえる
    要調整

--- Meteor/Sources/Nodes/Meteor.swift
+++ Meteor/Sources/Nodes/Meteor.swift
@@ -28,6 +28,8 @@ class Meteor: SKNode{
     override init(){
         self.texture = SKTexture(imageNamed: "normal_meteor")
         self.crack = SKSpriteNode(imageNamed: "ヒビ グレー 2")
+        self.crack.xScale = 0.5
+        self.crack.yScale = self.crack.xScale
         super.init()
         
     }
@@ -82,7 +84,16 @@ class Meteor: SKNode{
         //ヒビを追加
         self.crack.removeFromParent()//すでに追加されていた場合エラーになるため削除しておく
         self.crack.zPosition = meteor.zPosition + 0.1
-        meteor.addChild(crack)
+        //弱点に応じてxの位置を調整
+        switch self.weakPos {
+        case .left:
+            self.crack.position.x = -meteor.size.width * 0.25
+        case .center:
+            self.crack.position.x = 0
+        case .right:
+            self.crack.position.x = meteor.size.width * 0.25
+        }
+        meteor.addChild(self.crack)
         return meteor
     }
 
@@ -94,7 +105,7 @@ class Meteor: SKNode{
         self.HP[XPositon.right.hashValue] = maxHP
         //弱点にする位置を決めてそこのHPを1に書き換える
         if let xPos = XPositon.random{
-            var weakPos = xPos
+            self.weakPos = xPos
             self.HP[xPos.hashValue] = 1
         }
         print("HP:\(self.HP)")
